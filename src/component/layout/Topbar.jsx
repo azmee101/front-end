2f0bee2732185c91074106b180b0f25769424345
Azmee <azmeetofail4115@gmail.com>
2025-05-17 17:03:14 +0600

    nagad text color change

--- src/component/layout/Topbar.jsx
+++ src/component/layout/Topbar.jsx
@@ -1,7 +1,7 @@
 const Topbar = ({ user }) => {
   return (
     <header className="px-1 pt-4">
-      <nav className="bg-red-600 text-orange-400 shadow-lg h-12 rounded-lg">
+      <nav className="bg-red-600 text-white-400 shadow-lg h-12 rounded-lg">
         <div className="flex items-center justify-between h-full px-4">
           <div className="flex items-center">
             <img
@@ -9,14 +9,15 @@ const Topbar = ({ user }) => {
               src="Nagad_Vertical Logo_Latest 1.svg"
               alt="Nagad DFS Logo"
             />
-            <span className="text-xl font-semibold">Nagad</span>
+            <span className="text-xl font-semibold text-white">Nagad</span>
           </div>
           
-          <div className="hidden md:block ml-auto mr-4">
-            <span className="text-black">
-              Logged in as: {user.username}
-            </span>
-          </div>
+          <button 
+  type="button" 
+  className="hidden md:block ml-auto mr-4 bg-white rounded-md px-2 py-2 text-orange-500 hover:text-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+>
+  {user.username}
+</button>
         </div>
       </nav>
     </header>
